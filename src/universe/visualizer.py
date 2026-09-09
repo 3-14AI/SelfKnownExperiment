d2837409904a44b382f22d9cd405c68f52ed6585
pimenovdv <25670278+pimenovdv@users.noreply.github.com>
2026-09-09 15:32:10 +0000

    Add visualization for is_fire_dancer

--- src/universe/visualizer.py
+++ src/universe/visualizer.py
@@ -244,6 +244,8 @@ class CLIVisualizer:
                         char = 'q'
                     elif getattr(entity, 'has_blubber', False):
                         char = '@'
+                    elif getattr(entity, 'is_fire_dancer', False):
+                        char = 'F'
                     elif getattr(entity, 'is_mud_bather', False):
                         char = 'n'
                     elif getattr(entity, 'has_spikes', False):
